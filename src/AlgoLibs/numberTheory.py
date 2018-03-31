f0bc848d34727fe9881eb784e11b2ae66e9ceb73
yeguanghao <yeguanghao1999@gmail.com>
2018-03-31 16:28:53 -0700

    Remodel nt

--- src/AlgoLibs/numberTheory.py
+++ src/AlgoLibs/numberTheory.py
@@ -1,4 +1,9 @@
+threshold = 0
+prime_set = set()
+
+
 def prime_linear_sieve(n):
+
     is_composite = [False] * (n + 1)
     primes = list()
     for i in range(2, n + 1):
@@ -10,16 +15,26 @@ def prime_linear_sieve(n):
             is_composite[j * i] = True
             if i % j == 0:
                 break
+    global threshold
+    global prime_set
+    if n > threshold:
+        threshold = n
+        prime_set = set(primes)
     return primes
 
 
 def is_prime_naive(k):
-    if k < 2:
-        return False
+    global threshold
+    global prime_set
+    if k < threshold:
+        return k in prime_set
     else:
-        i = 2
-        while i * i <= k:
-            if k % i == 0:
-                return False
-            i += 1
-        return True
+        if k < 2:
+            return False
+        else:
+            i = 2
+            while i * i <= k:
+                if k % i == 0:
+                    return False
+                i += 1
+            return True
